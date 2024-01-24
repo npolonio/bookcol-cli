10395ec988438fdc8d368440c33242e3f6d1683a
Nicolau Polonio <nicolau.polonio@gmail.com>
2024-01-24 14:54:34 -0300

    Update input_validator.py

--- warehouse/input_validator.py
+++ warehouse/input_validator.py
@@ -1,19 +1,25 @@
 import re
 import click
+import logging
+
+logging.basicConfig(filename='inventory.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class InputValidator: 
     @staticmethod
     def validation(expression, error_message):
-        if expression: return True
+        if expression: 
+            return True
         
-        click.echo(error_message)
+        click.echo(f'Error: {error_message}')
+        logging.error(error_message)
         return False
                 
     @staticmethod   
     def validate_id(input_id):
         is_valid = re.match(r"^\d{4}$", str(input_id))
         if not is_valid:
-            click.echo('Invalid ID. Please provide a 4-digit number')
+            click.echo('Error: Invalid ID. Please provide a 4-digit number')
+            logging.error(f"Invalid ID: {input_id}")
         return is_valid
 
     @staticmethod
@@ -34,4 +40,4 @@ class InputValidator:
             return InputValidator.validate_quantity(input_value)
         elif input_attribute == "price": 
             return InputValidator.validate_price(input_value)
-
+            
