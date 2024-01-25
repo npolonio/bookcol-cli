36354b9c1c338f015e713f52607f5bee5065fcef
Nicolau Polonio <nicolau.polonio@gmail.com>
2024-01-24 21:44:08 -0300

    Update product.py

--- warehouse/product.py
+++ warehouse/product.py
@@ -1,22 +1,25 @@
 import click
-#Do I need a __str__, a get_dict and a format_output?
+
 class Product:
-    def __init__(self, _id, name, quantity, price, location):
+    def __init__(self, _id: int, name: str, quantity: int, price: float, location: str):
         self._id = _id
         self.name = name
         self.quantity = quantity
         self.price = price
         self.location = location
 
+
     def __str__(self):
         return f'{self._id}, {self.name}, {self.quantity}, {self.price}, {self.location}'
 
-    @property
-    def id(self):
-        return self._id
 
     @property
-    def get_dict(self):
+    def id(self) -> int:
+        return self._id
+
+
+    @property
+    def to_dict(self) -> dict:
         return {
             "id": self._id,
             "name": self.name,
@@ -24,6 +27,7 @@ class Product:
             "price": self.price,
             "location": self.location
         }
+
     
     def format_output(self): 
         click.echo(f"Product found with ID {self.id}:")
@@ -32,7 +36,9 @@ class Product:
         click.echo(f"Product Price: {self.price}")
         click.echo(f"Product Location: {self.location}")
 
+
     @classmethod
-    def create_from_dict(cls, data):
+    def create_from_dict(cls, data: dict) -> 'Product':
         return cls(data['id'], data['name'], data['quantity'],
                    data['price'], data['location'])
+
