f4bc6de3e15824df453692bdbaa37a44efaa9d9b
Nicolau Polonio <nicolau.polonio@gmail.com>
2024-02-02 15:44:30 -0300

    Update __main__.py

--- bookcol/bookcol/__main__.py
+++ bookcol/bookcol/__main__.py
@@ -1,6 +1,5 @@
 import click
-import inquirer
-from .db import setup_database
+from .commands import menu
 from . import commands
 
 
@@ -19,46 +18,6 @@ cli.add_command(commands.display)
 cli.add_command(commands.filter)
 cli.add_command(commands.backup)
 cli.add_command(commands.restore)
-
-
-def menu():
-    questions = [
-        inquirer.List('menu_choice',
-                      message='Choose an action:',
-                      choices=[
-                          'Add a book',
-                          'Alter a book',
-                          'Delete a book',
-                          'Search for a book',
-                          'Display all books',
-                          'Filter books',
-                          'Backup collection',
-                          'Restore collection',
-                          'Exit'
-                      ]),
-    ]
-
-    answer = inquirer.prompt(questions)
-
-    if answer['menu_choice'] == 'Add a book':
-        cli(['add'])
-    elif answer['menu_choice'] == 'Alter a book':
-        cli(['alter'])
-    elif answer['menu_choice'] == 'Delete a book':
-        cli(['delete'])
-    elif answer['menu_choice'] == 'Search for a book':
-        cli(['search'])
-    elif answer['menu_choice'] == 'Display all books':
-        cli(['display'])
-    elif answer['menu_choice'] == 'Filter books':
-        cli(['filter'])
-    elif answer['menu_choice'] == 'Backup collection':
-        cli(['backup'])
-    elif answer['menu_choice'] == 'Restore collection':
-        cli(['restore'])
-    elif answer['menu_choice'] == 'Exit':
-        click.echo('Goodbye!')
-        exit()
 
 
 if __name__ == "__main__":
