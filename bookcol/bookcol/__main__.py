aaaf470c4c25b0bbf514323453c6c175e510a14b
Nicolau Polonio <nicolau.polonio@gmail.com>
2024-02-07 22:20:18 -0300

    Update __main__.py

--- bookcol/bookcol/__main__.py
+++ bookcol/bookcol/__main__.py
@@ -6,6 +6,20 @@ from . import commands
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
+    """
+    Command-line interface for managing a book collection.
+
+    This CLI tool provides various commands for interacting with a book collection,
+    such as adding, altering, deleting, searching, displaying, filtering, backing up,
+    and restoring books. If no specific command is provided, it will display a menu
+    with available options.
+
+    Args:
+        ctx (click.Context): The context object representing the current invocation.
+
+    Returns:
+        None
+    """
     if ctx.invoked_subcommand is None:
         menu()
 
